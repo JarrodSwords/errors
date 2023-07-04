da2f37690927f11a961374f8e6fcdaed2fce6b8d
Jarrod Swords <jarrod.swords@gmail.com>
2023-07-04 18:55:46 -0400

    add result

---
+++ errors-spec/Results/WhenInstantiating.cs
@@ -0,0 +1,18 @@
+﻿using Jgs.Errors.Results;
+
+namespace Jgs.Errors.Spec.Results;
+
+public class WhenInstantiating
+{
+    #region Requirements
+
+    [Fact]
+    public void ThenReturnSuccess()
+    {
+        var result = Result.Success();
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    #endregion
+}
---
+++ errors/Results/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Jgs.Errors.Results;
+
+public class Result
+{
+    #region Implementation
+
+    public bool IsSuccess { get; } = true;
+
+    #endregion
+
+    #region Static Interface
+
+    public static Result Success() => new();
+
+    #endregion
+}
